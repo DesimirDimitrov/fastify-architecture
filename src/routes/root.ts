0f2473582cc9c884180c75f49c708a72f4691499
DesimirDimitrov <2309808+DesimirDimitrov@users.noreply.github.com>
2023-01-16 22:43:32 +0200

    Update root.ts

--- src/routes/root.ts
+++ src/routes/root.ts
@@ -2,7 +2,7 @@ import { FastifyPluginAsync } from "fastify";
 
 const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
   fastify.get("/", async function(request, reply) {
-    return { message: "success" };
+    return { message: "success :)" };
   });
 };
 
