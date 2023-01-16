9349fb82445f0f95e023bed0fd857af140196fe6
Desimir Dimitrov <desko007@gmail.com>
2023-01-16 23:05:57 +0200

    Ecosystem config changes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,9 +2,9 @@ module.exports = {
   apps: [
     {
       name: "FastifyLamungiAPI",
-      exec_mode: "fork",
+      exec_mode: "cluster",
       instances: "1",
-      script: "npm run build && node /home/desimir/actions-runner/fastifylamungiapi/fastify-architecture/fastify-architecture/dist/server.js", // your script
+      script: "npm run build && node dist/server.js", // your script
       args: "start",
       env: {
         NODE_ENV: "production",
