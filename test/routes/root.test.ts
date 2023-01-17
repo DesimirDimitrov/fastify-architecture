da9abdc569e3f122a9308b6109075c91c888a691
Desimir Dimitrov <desko007@gmail.com>
2023-01-17 21:46:47 +0200

    Minor changes

--- test/routes/root.test.ts
+++ test/routes/root.test.ts
@@ -1,19 +1,19 @@
-describe('route', async () => {
-	it('GET `/`', async ({fastify}) => {
-		const response = await fastify.inject({
-			method: 'GET',
-			url: '/',
-		})
+describe("route", async () => {
+  it("GET `/`", async ({ fastify }) => {
+    const response = await fastify.inject({
+      method: "GET",
+      url: "/"
+    });
 
-		expect(response.statusCode).toEqual(404)
-	})
+    expect(response.statusCode).toEqual(404);
+  });
 
-	it('GET `/api`', async ({fastify}) => {
-		const response = await fastify.inject({
-			method: 'GET',
-			url: '/api',
-		})
+  it("GET `/api`", async ({ fastify }) => {
+    const response = await fastify.inject({
+      method: "GET",
+      url: "/api"
+    });
 
-		expect(response.json()).toEqual({message: "success"})
-	})
-})
+    expect(response.json()).toEqual({ message: "success" });
+  });
+});
